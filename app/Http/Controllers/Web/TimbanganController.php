e2a7861f6d78de82711e4c3b7ac66c281c8ca319
randika <randikarudiadidisastra@gmail.com>
2023-12-26 17:35:03 +0700

    fix: detail

--- app/Http/Controllers/Web/TimbanganController.php
+++ app/Http/Controllers/Web/TimbanganController.php
@@ -106,7 +106,7 @@ class TimbanganController extends Controller
             // Surat Jalan
             $data['suratJalan'] = Letter::with([
                 'timbangans' => function ($query) {
-                    $query->join('barangs', 'barangs.kode', 'timbangans.kode_barang');
+                    $query->join('barangs', 'barangs.kode', 'timbangans.kode_barang')->orderBy('barangs.name', 'ASC');
                 }, 'customers'
             ])->where('id_transport', $data['transport']->id)->orderBy('id', 'ASC')->get();
 
@@ -128,7 +128,7 @@ class TimbanganController extends Controller
             // Surat Jalan
             $data['suratJalan'] = Letter::with([
                 'timbangans' => function ($query) {
-                    $query->join('barangs', 'barangs.kode', 'timbangans.kode_barang');
+                    $query->join('barangs', 'barangs.kode', 'timbangans.kode_barang')->orderBy('barangs.name', 'ASC');
                 }, 'customers'
             ])->where('id_transport', $data['transport']->id)->orderBy('id', 'ASC')->get();
 
